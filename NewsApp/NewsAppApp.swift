6514917cf065d3c6141a65ba533434a62390d33f
MelnikovAleksandr <melnikovjavadev@gmail.com>
2025-11-20 20:03:31 -0800

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Александр Мельников on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
